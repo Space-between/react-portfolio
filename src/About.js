009767a8654d7f1049006a5add482b377d392741
gray <xodyd1756@naver.com>
2020-08-03 02:24:54 +0900

    react-portfolio

--- src/About.js
+++ src/About.js
@@ -9,10 +9,11 @@ export default function About() {
         About me
       </div>
       <div className="mx-20 mt-10 text-gray-700 text-xl">
-        <p>
+        <p className="aboutme-font-length">
           안녕하세요. 프론트엔드 개발자 김태용입니다.
           <br />
           웹 퍼블리셔를 시작으로 프론트엔드 개발자에 이르기까지 매우 많은 시간과 노력을 웹 개발에 기울였고 지금보다 더 향상된 실력의 개발자가 되기 위해서 끊임없이 발전하고 있습니다.
+          <br />
           <br />
           웹 퍼블리셔 출신 프론트엔드 개발자답게 PC 페이지부터 Mobile 페이지, 반응형 웹 페이지, 그리고 웹 애플리케이션, 게임과 같은 웹 서비스까지 다양한 기술들을 활용하여 개발한 경험이 있습니다.
           <br />
