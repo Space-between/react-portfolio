7af45ac76a5d753310af335a24dbaa4684bae62f
gray <xodyd1756@naver.com>
2020-08-04 14:39:52 +0900

    React Portfolio

--- src/ProjectB.js
+++ src/ProjectB.js
@@ -14,10 +14,10 @@ export default function ProjectA() {
           <div className="w-2/3 flex flex-col justify-center">
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2 text-gray-800">
-                React Web Game
+               Web Game + Contact
               </div>
               <p className="text-gray-700 text-lg mb-5">
-                React로 만든 웹 게임입니다. 끝말잇기와 같은 비교적 간단한 게임부터 지뢰찾기, 숫자야구게임같은 복잡한 게임까지 총 7가지의 게임을 React로 만들었습니다.
+                React로 만든 웹 게임과 주소록입니다. 끝말잇기, 로또 생성기, 지뢰 찾기, 숫자야구게임 등 총 7가지의 게임과 전화번호를 추가, 수정 및 제거할 수 있는 기능을 갖고 있는 주소록을 React로 만들었습니다.
                 <br />
                 <br />
                 <div className="font-bold text-gray-800">Feature</div>
@@ -25,14 +25,14 @@ export default function ProjectA() {
               </p>
               <a
                 className="flex-shrink-0 bg-teal-500 hover:bg-teal-400 border-teal-500 hover:border-teal-400 text-sm border-4 text-white py-1 px-2 rounded"
-                href="https://space-between.github.io/react-web-game/"
+                href="https://space-between.github.io/webgame-contact/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                React Web Game 바로가기
+                Web Game + Contact 바로가기
               </a>
               <a
-                href="https://github.com/Space-between/react-web-game"
+                href="https://github.com/Space-between/webgame-contact"
                 rel="noopener noreferrer"
                 target="_blank"
                 className="ml-3 text-teal-500 cursor-pointer hover:text-teal-400"
